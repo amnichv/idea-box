23bf63e8d50735453f6f27ef712a411cf2108194
Amanda C <amandan.chavez@gmail.com>
2017-02-14 14:36:20 -0700

    organize js, add functions, change classes in html

--- main.js
+++ main.js
@@ -4,27 +4,52 @@
 // var ideaInput = $('.idea')
 // var newIdea = []
 
-function Idea(id, title, body, quality) {
+// function Idea(id, title, body, quality) {
+//   this.id = id;
+//   this.title = title;
+//   this.body = body;
+//   this.quality = quality;
+// }
+
+function Idea(title, body, id, quality) {
   this.id = id;
   this.title = title;
   this.body = body;
-  this.quality = quality;
+  this.quality = quality || 'swill';
 }
+
+function displayIdea (aHa) {
+  var displayHTML = '<div class="new-card">';
+  displayHTML += '<h2 class="title">' + aHa.title + '<h2>';
+  displayHTML += '<p class="body">' + aHa.body + '</p>';
+  displayHTML += `<button class="delete"></button>
+                 <button class="up-vote"></button>
+                 <button class="down-vote"></button>`;
+  displayHTML += '<p class="quality">quality: ' + aHa.quality + '</p></div>';
+
+$('.idea-card-container').prepend(displayHTML);
+}
+
+function upQuality () {
+  //fill in
+  $('.up-vote').on('click', function () {
+  //if quality === swill
+  //quality should upgrade to plausible
+  //if quality === plausible
+  //upgrade quality to genius
+  })
+}
+
+
+
 
 $('.save-btn').on('click', function () {
-  
-}
-
-function printIdeaCard () {
   var $userTitle = $('.title').val();
-  var $userIdea = $('.idea').val();
-  $('.idea-card-container').prepend(
-    `<div class="new-card"</div>
-    <h1>title goes here</h1>${titleInput.val()}
-    <p>${ideaInput.val()}</p>
-    <button class="delete"></button>
-    <button class="upVote"></button>
-    <button class="downVote"></button>
-    <p>quality</p>`
-  )
-}
+  console.log($userTitle);
+  var $userBody = $('.body').val();
+  console.log($userBody);
+
+  var aHa = new Idea($userTitle, $userBody);
+  console.log(aHa);
+  displayIdea(aHa);
+})
